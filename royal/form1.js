4ea5638e3c37fc59ac74633a6a8025deea706bf6
Cafiifa-Mustafa <cafiifahariir@gmail.com>
2023-04-03 16:36:24 +0200

    royal

---
+++ royal/form1.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+const propertySchema = new mongoose.Schema(
+  {
+    order_ID: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    full_name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: string,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    payment: {
+      type: Number,
+      required: true,
+    },
+    payment_type: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+    featured: {
+      type: Boolean,
+      default: false,
+    },  
+  },
+  { timestamps: true }
+);
+export default mongoose.model('form1', form1Schema);
