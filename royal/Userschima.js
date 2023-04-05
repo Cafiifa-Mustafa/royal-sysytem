18e5d1aa3a4476db8d54f4b23df71d763b91e8d5
Cafiifa-Mustafa <cafiifahariir@gmail.com>
2023-04-05 13:30:34 +0200

    feat added Userschima

---
+++ royal/Userschima.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+        type: String,
+        required: true, 
+      },
+      phone: {
+        type: Number,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      Gender: {
+        type: String,
+        required: true,
+      },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    confirm: {
+        type: String,
+        required: true,
+      },
+    role: {
+      type: String,
+      default: "user",
+    },
+    photo: {
+        type: String,
+      },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("User", userSchema);
